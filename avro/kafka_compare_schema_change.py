105913414eaa9005eab1a0514e88e3b5617b6b2a
Girish Mallula <chandramgc@gmail.com>
2023-02-15 20:04:47 -0700

    c

--- avro/kafka_compare_schema_change.py
+++ avro/kafka_compare_schema_change.py
@@ -1,7 +1,7 @@
-import os
 import json
 from collections import deque
 from avro.schema import parse
+import os
 
 def compare_schemas(schema1, schema2, path=""):
     # Convert the schemas to dicts
@@ -11,10 +11,8 @@ def compare_schemas(schema1, schema2, path=""):
     # Initialize a queue to traverse the schema recursively
     queue = deque([("", schema_dict1, schema_dict2)])
 
-    # Initialize lists to store the fields that have changed
+    # Initialize a list to store the changed fields
     changed_fields = []
-    deleted_fields = []
-    added_fields = []
 
     # Traverse the schema recursively
     while queue:
@@ -25,34 +23,34 @@ def compare_schemas(schema1, schema2, path=""):
             # Check if the field types are the same
             if field1["type"] != field2["type"]:
                 changed_fields.append(path + field1["name"])
-            
+
             # Recurse on child fields
             if "fields" in field1 and "fields" in field2:
                 for child1, child2 in zip(field1["fields"], field2["fields"]):
                     queue.append((path + field1["name"] + ".", child1, child2))
             elif "fields" in field1:
                 for child1 in field1["fields"]:
-                    deleted_fields.append(path + field1["name"] + "." + child1["name"])
+                    queue.append((path + field1["name"] + ".", child1, {"name": ""}))
             elif "fields" in field2:
                 for child2 in field2["fields"]:
-                    added_fields.append(path + field2["name"] + "." + child2["name"])
+                    queue.append((path + field2["name"] + ".", {"name": ""}, child2))
         else:
             # This field has been deleted or added
             if path == "":
                 # The field is at the top level
                 if field1["name"] in schema_dict1:
-                    deleted_fields.append(field1["name"])
+                    queue.append((field1["name"] + ".", field1, {"name": ""}))
                 else:
-                    added_fields.append(field2["name"])
+                    queue.append((field2["name"] + ".", {"name": ""}, field2))
             else:
                 # The field is nested
                 if field1["name"] in schema_dict1[path[:-1]]["fields"]:
-                    deleted_fields.append(path + field1["name"])
+                    queue.append((path + field1["name"] + ".", field1, {"name": ""}))
                 else:
-                    added_fields.append(path + field2["name"])
+                    queue.append((path + field2["name"] + ".", {"name": ""}, field2))
 
-    # Return the changed, deleted, and added fields
-    return changed_fields, deleted_fields, added_fields
+    # Return the changed fields
+    return changed_fields
 
 # Construct the file paths relative to the current working directory
 schema1_path = os.path.join('avro','files','schema-1.avsc')
@@ -66,7 +64,5 @@ with open(schema2_path, "r") as f:
     schema2 = parse(f.read())
 
 # Compare the schemas and print the results
-changed, deleted, added = compare_schemas(schema1, schema2)
+changed = compare_schemas(schema1, schema2)
 print("Changed fields:", changed)
-print("Deleted fields:", deleted)
-print("Added fields:", added)
